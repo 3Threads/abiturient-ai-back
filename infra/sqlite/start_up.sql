08121b2eb14d5c2bdc64981c1eb37ae39ea7f512
DachiGoshadze <dgosh21@freeuni.edu.ge>
2024-02-07 23:29:02 +0400

    sw

--- infra/sqlite/start_up.sql
+++ infra/sqlite/start_up.sql
@@ -7,7 +7,8 @@ CREATE TABLE TASKS
     ID           INTEGER PRIMARY KEY AUTOINCREMENT,
     TASK_NUM     INTEGER      NOT NULL,
     TASK_TITLE   TEXT         NOT NULL,
-    TASK_TEXT    TEXT         NOT NULL,
+    TASK_TEXT    TEXT,
+    TASK_OPTIONS  TEXT,
     POINT        INTEGER      NOT NULL,
     TASK_TYPE    TEXT         NOT NULL,
     YEAR         INTEGER TEXT NOT NULL,
@@ -16,14 +17,34 @@ CREATE TABLE TASKS
     QUESTIONS_ID TEXT         NOT NULL
 );
 
-CREATE TABLE QUESTIONS
+CREATE TABLE MultipleChoiceQuestion
 (
     QUESTION_ID       INTEGER PRIMARY KEY AUTOINCREMENT,
-    QUESTION_TYPE     TEXT NOT NULL,
-    QUESTION_IMG_LINK TEXT,
     QUESTION_TEXT     TEXT,
     QUESTION_OPTIONS  TEXT,
-    QUESTION_ANSWERS  TEXT
+    QUESTION_ANSWER  TEXT
+);
+
+
+CREATE TABLE FillWithArticlesQuestion
+(
+    QUESTION_ID       INTEGER PRIMARY KEY AUTOINCREMENT,
+    CORRECT_ANSWER  TEXT
+);
+
+
+CREATE TABLE TitlingQuestion
+(
+    QUESTION_ID       INTEGER PRIMARY KEY AUTOINCREMENT,
+    PARAGRAPH TEXT,
+    CORRECT_ANSWERS  TEXT
+);
+
+
+CREATE TABLE FillTextQuestion
+(
+    QUESTION_ID       INTEGER PRIMARY KEY AUTOINCREMENT,
+    CORRECT_ANSWER  TEXT
 );
 
 INSERT INTO TASKS (TASK_NUM, TASK_TITLE, TASK_TEXT, POINT, TASK_TYPE, YEAR, VARIANT, SUBJECT, QUESTIONS_ID)
@@ -35,29 +56,61 @@ answer A, B or C. You have 20 seconds to look through the task. You will hear th
         'ListeningTask',
         2021,
         1,
-        'english',
+        'English',
         '1#2#3');
-INSERT INTO QUESTIONS (QUESTION_TYPE, QUESTION_IMG_LINK, QUESTION_TEXT, QUESTION_OPTIONS, QUESTION_ANSWERS)
-VALUES ('MultipleChoiceQuestion',
-        NULL,
-        'What does Sandro want to be?',
+INSERT INTO MultipleChoiceQuestion (QUESTION_TEXT, QUESTION_OPTIONS, QUESTION_ANSWER)
+VALUES ('What does Sandro want to be?',
         'A doctor.#An actor.#An economist.',
         'A doctor.');
-INSERT INTO QUESTIONS (QUESTION_TYPE, QUESTION_IMG_LINK, QUESTION_TEXT, QUESTION_OPTIONS, QUESTION_ANSWERS)
-VALUES ('MultipleChoiceQuestion',
-        NULL,
-        'Where does Sandro work? ',
+INSERT INTO MultipleChoiceQuestion (QUESTION_TEXT, QUESTION_OPTIONS, QUESTION_ANSWER)
+VALUES ('Where does Sandro work? ',
         'In a bank.#In a theatre.#In a bookshop.',
         'In a bookshop.');
-INSERT INTO QUESTIONS (QUESTION_TYPE, QUESTION_IMG_LINK, QUESTION_TEXT, QUESTION_OPTIONS, QUESTION_ANSWERS)
-VALUES ('MultipleChoiceQuestion',
-        NULL,
-        'Where does Sandro work? ',
+INSERT INTO MultipleChoiceQuestion (QUESTION_TEXT, QUESTION_OPTIONS, QUESTION_ANSWER)
+VALUES ('Where does Sandro work? ',
         'In a bank.#In a theatre.#In a bookshop.',
         'In a bookshop.');
-INSERT INTO QUESTIONS (QUESTION_TYPE, QUESTION_IMG_LINK, QUESTION_TEXT, QUESTION_OPTIONS, QUESTION_ANSWERS)
-VALUES ('MultipleChoiceQuestion',
-        NULL,
-        'Agatha Christie is called the ‘Queen of Crime’ because sh',
-        'used to work as a detective.#is the author of popular books on crime.#is the most translated author of the books on crime',
-        'is the author of popular books on crime.');
+
+INSERT INTO TASKS (TASK_NUM, TASK_TITLE, TASK_TEXT, POINT, TASK_TYPE, YEAR, VARIANT, SUBJECT, QUESTIONS_ID)
+VALUES (2,
+        'Read the questions (1-8) and find the answers to them in the paragraphs (A-F) of the text. Some
+paragraphs correspond to more than one question.'
+        ,
+        '1. explains how Harrods attracted rich customers?#
+        2. mentions a cultural event which had a positive effect on Harrods?#
+        3. states how customers should dress when going to Harrods?#
+        4. gives the date when the Harrods building was totally destroyed?#
+        5. gives the number of people currently employed by Harrods?#
+        6. mentions the staircase built to carry people between the floors?',
+        8,
+        'TitlingTask',
+        2021,
+        1,
+        'English',
+        '1#2#3');
+INSERT INTO TitlingQuestion (PARAGRAPH, CORRECT_ANSWERS) VALUES (
+                                                                 'A. Harrods department store is one of the most famous shops in London with millions of people visiting each year. In the
+beginning, though, Harrods was just a small shop in a single room in Stepney, East London. The shop sold only tea and groceries.
+A young tea merchant, Charles Henry Harrod opened it in 1824 when he was only 25 years old. Besides himself, Charles Henry
+Harrod employed two assistants and a messenger boy*. In 1849 the store moved to the Knightsbridge area of London and
+expanded. Just two years later, the Great Exhibition of 1851 brought many visitors to Knightsbridge. This was a great change
+because, as a result, Harrods attracted more customers and enjoyed great success.',
+                                                                 '1#3#4'
+                                                                );
+INSERT INTO TitlingQuestion (PARAGRAPH, CORRECT_ANSWERS) VALUES (
+                                                                 'B. Harrods steadily grew, and by 1873 the name ‘Harrod’s Store’ appeared at the front of the shop. Over several years the shop
+got bigger and started selling fruit, vegetables and furniture. By 1883 Harrods had grown to six departments across five floors,
+with over 200 assistants. It started to offer its customers everything from medicines and perfumes to clothing and food. The
+department store became well known for its high-quality products and excellent personalised service. This way it managed to
+reach out to wealthy customers who were willing to spend more money for better quality. ',
+                                                                 '2#5'
+                                                                );
+INSERT INTO TitlingQuestion (PARAGRAPH, CORRECT_ANSWERS) VALUES (
+                                                                 'C. Then, on the night of December 7, 1883, the store unexpectedly caught fire. The entire building burnt down to the ground. But
+instead of closing down, the store moved across the street and an architect was hired to build a newer, grander building. Despite
+the tragedy, all Christmas orders were fulfilled and the store’s reputation was not only saved but also improved. The store reopened
+the following year. In 1898, Harrods installed England’s first ‘moving stairs’ that we now call an escalator. The first escalator was
+considered a frightening experience, so nervous customers were offered brandy - an alcoholic drink - at the top floor to calm them
+down.',
+                                                                 '6'
+                                                                )
--- infra/sqlite/tasks.py
+++ infra/sqlite/tasks.py
@@ -10,7 +10,8 @@ from core.question import (
     EmailQuestion,
     EssayQuestion,
 )
-from core.task import Task, TitlingTask, ListeningTask, ReadAndWriteTask, FillTextTask
+from core.task import Task, TitlingTask, ListeningTask, ReadAndWriteTask, FillTextTask, EssayTask, EmailTask, \
+    FillWithArticlesTask
 
 
 @dataclass
@@ -29,6 +30,7 @@ class TasksDatabase:
                 task_num,
                 task_title,
                 task_text,
+                task_options,
                 point,
                 task_type,
                 year,
@@ -36,72 +38,58 @@ class TasksDatabase:
                 subject,
                 questions_id,
         ) in tasks_info:
-            st = ""
-            list_of_questions_id = str(questions_id).split("#")
-            for id in list_of_questions_id:
-                st += id
-                st += ","
-            st = st[: (len(st) - 1)]
-            questions_info = self.cur.execute(
-                "SELECT * FROM QUESTIONS WHERE QUESTION_ID IN (" + st + ")"
-            ).fetchall()
-
-            print(questions_info)
             questions = []
-            for question_info in questions_info:
-                questions.append(self.get_question(question_info))
             match task_type:
                 case "TitlingTask":
-                    task_type = TitlingTask(questions[0])
+                    questions_info = self.get_questions_info("TitlingQuestion", questions_id)
+                    print(questions_info)
+                    for id, paragraph, correct_answers in questions_info:
+                        question = TitlingQuestion(paragraph, correct_answers.split('#'))
+                        questions.append(question)
+                    task_type = TitlingTask(questions, task_text.split('#'))
                 case "ReadAndWriteTask":
-                    task_type = ReadAndWriteTask(task_text, questions)
+                    questions_info = self.get_questions_info("MultipleChoiceQuestion", questions_id)
+                    for id, question_text, question_options, question_answer in questions_info:
+                        question = MultipleChoiceQuestion(question_text, question_options.split('#'),
+                                                          question_answer.split('#'))
+                        questions.append(question)
+                    task_type = ReadAndWriteTask(questions, task_text)
                 case "FillTextTask":
-                    task_type = FillTextTask(questions[0])
+                    questions_info = self.get_questions_info("FillTextQuestion", questions_id)
+                    for id, question_answer in questions_info:
+                        question = FillTextQuestion(question_answer)
+                        questions.append(question)
+                    task_type = FillTextTask(questions, task_text, task_options.splite('#'))
                 case "FillWithArticlesTask":
-                    task_type = FillTextTask(questions[0])
+                    questions_info = self.get_questions_info("FillWithArticlesQuestion", questions_id)
+                    for id, question_answer in questions_info:
+                        question = FillWithArticlesQuestion(question_answer.split('#'))
+                        questions.append(question)
+                    task_type = FillWithArticlesTask(questions, task_text)
                 case "EmailTask":
-                    task_type = FillTextTask(questions[0])
+                    email_question = EmailQuestion()
+                    task_type = EmailTask([email_question], task_text)
                 case "EssayTask":
-                    task_type = FillTextTask(questions[0])
+                    essey_question = EssayQuestion()
+                    task_type = EssayTask([essey_question], task_text)
                 case "ListeningTask":
+                    questions_info = self.get_questions_info("MultipleChoiceQuestion", questions_id)
+                    for id, question_text, question_options, question_answer in questions_info:
+                        question = MultipleChoiceQuestion(question_text, question_options.split('#'),
+                                                          question_answer.split('#'))
+                        questions.append(question)
                     task_type = ListeningTask(questions)
             task = Task(task_num, task_title, point, task_type, task_type)
             tasks.append(task)
         return tasks
 
-    def get_question(self, question_info) -> QuestionType:
-        (
-            question_id,
-            question_type,
-            question_img_link,
-            question_text,
-            question_options,
-            questions_answers,
-        ) = question_info
-        match question_type:
-            case "MultipleChoiceQuestion":
-                options = question_options.split("#")
-                return MultipleChoiceQuestion(question_text, options, questions_answers)
-            case "TitlingQuestion":
-                paragraphs = question_text.split("#")
-                titles = question_options.split("#")
-                answers = questions_answers.split("###")
-                correct_answers: dict[int : list[str]] = {}
-                for an in answers:
-                    paragraph_index, answers = an.splite("##")
-                    correct_answers[int(paragraph_index)] = list(answers.splite("#"))
-                return TitlingQuestion(titles, paragraphs, correct_answers)
-            case "FillTextQuestion":
-                options = question_options.split("#")
-                answers = questions_answers.split("#")
-                return FillTextQuestion(question_text, options, answers)
-            case "FillWithArticlesQuestion":
-                answers = questions_answers.split("##")
-                correct_answers = []
-                for answ in answers:
-                    correct_answers.append(answers(answ.split("#")))
-                return FillWithArticlesQuestion(question_text, correct_answers)
-            case "EmailQuestion":
-                return EmailQuestion(question_img_link)
-            case "EssayQuestion":
-                return EssayQuestion(question_text)
+    def get_questions_info(self, question_type: str, question_ids: str):
+        st = ""
+        list_of_questions_id = str(question_ids).split("#")
+        for id in list_of_questions_id:
+            st += id
+            st += ","
+        st = st[: (len(st) - 1)]
+        questions_info = self.cur.execute(
+                "SELECT * FROM "+ question_type +" WHERE QUESTION_ID IN (" + st + ")").fetchall()
+        return questions_info
