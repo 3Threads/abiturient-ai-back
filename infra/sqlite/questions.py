b0273171e446a51a45197f3b1eab2ed7bb37265b
GuRgEnA <agurg21@freeuni.edu.ge>
2024-03-05 21:58:26 +0400

    ctrl alt l missing

--- infra/sqlite/questions.py
+++ infra/sqlite/questions.py
@@ -9,16 +9,26 @@ class MultipleChoiceQuestionDataBase:
     con: Connection
     cur: Cursor
 
-    def insert_multiple_choice_question(self, task_id: int, question: str, options: list[str], answer: str):
+    def insert_multiple_choice_question(
+        self, task_id: int, question: str, options: list[str], answer: str
+    ):
         question_options = options[0]
         for i in range(len(options)):
             if i != 0:
                 question_options = question_options + f"#{options[i]}"
-        self.cur.execute("INSERT INTO MultipleChoiceQuestion(TASK_ID, QUESTION_TEXT, QUESTION_OPTIONS, QUESTION_ANSWER) VALUES (?, ?, ?, ?)", (task_id, question, question_options, answer))
+        self.cur.execute(
+            "INSERT INTO MultipleChoiceQuestion(TASK_ID, QUESTION_TEXT, QUESTION_OPTIONS, QUESTION_ANSWER) VALUES (?, ?, ?, ?)",
+            (task_id, question, question_options, answer),
+        )
         self.con.commit()
 
-    def get_multiple_choice_questions(self, task_id: int) -> list[MultipleChoiceQuestion]:
-        return self.cur.execute("SELECT * FROM MultipleChoiceQuestion WHERE TASK_ID = ?", (task_id,)).fetchall()
+    def get_multiple_choice_questions(
+        self, task_id: int
+    ) -> list[MultipleChoiceQuestion]:
+        return self.cur.execute(
+            "SELECT * FROM MultipleChoiceQuestion WHERE TASK_ID = ?", (task_id,)
+        ).fetchall()
+
 
 @dataclass
 class FillWithArticlesQuestionDataBase:
@@ -30,11 +40,20 @@ class FillWithArticlesQuestionDataBase:
         for i in range(len(answers)):
             if i != 0:
                 answer = answer + f"#{answers[i]}"
-        self.cur.execute("INSERT INTO FillWithArticlesQuestion(TASK_ID, CORRECT_ANSWER) VALUES (?, ?)", (task_id, answer))
+        self.cur.execute(
+            "INSERT INTO FillWithArticlesQuestion(TASK_ID, CORRECT_ANSWER) VALUES (?, ?)",
+            (task_id, answer),
+        )
         self.con.commit()
 
-    def get_fill_with_articles_questions(self, task_id: int, question_id: int) -> list[FillTextQuestion]:
-        return self.cur.execute("SELECT * FROM FillWithArticlesQuestion WHERE TASK_ID = ? AND QUESTION_ID = ?", (task_id, question_id)).fetchall()
+    def get_fill_with_articles_questions(
+        self, task_id: int, question_id: int
+    ) -> list[FillTextQuestion]:
+        return self.cur.execute(
+            "SELECT * FROM FillWithArticlesQuestion WHERE TASK_ID = ? AND QUESTION_ID = ?",
+            (task_id, question_id),
+        ).fetchall()
+
 
 @dataclass
 class TitlingQuestionDataBase:
@@ -46,11 +65,19 @@ class TitlingQuestionDataBase:
         for i in range(len(answers)):
             if i != 0:
                 answer = answer + f"#{answers[i]}"
-        self.cur.execute("INSERT INTO TitlingQuestion(TASK_ID, TITLE, CORRECT_ANSWERS) VALUES (?, ?, ?)", (task_id, title, answer))
+        self.cur.execute(
+            "INSERT INTO TitlingQuestion(TASK_ID, TITLE, CORRECT_ANSWERS) VALUES (?, ?, ?)",
+            (task_id, title, answer),
+        )
         self.con.commit()
 
-    def get_titling_questions(self, task_id: int, question_id: int) -> list[OpenQuestion]:
-        return self.cur.execute("SELECT * FROM TitlingQuestion WHERE TASK_ID = ? AND QUESTION_ID = ?", (task_id, question_id)).fetchall()
+    def get_titling_questions(
+        self, task_id: int, question_id: int
+    ) -> list[OpenQuestion]:
+        return self.cur.execute(
+            "SELECT * FROM TitlingQuestion WHERE TASK_ID = ? AND QUESTION_ID = ?",
+            (task_id, question_id),
+        ).fetchall()
 
 
 @dataclass
@@ -59,17 +86,22 @@ class FillTextQuestionDataBase:
     cur: Cursor
 
     def insert_fill_text_question(self, task_id: int, question_id: int, answer: str):
-        self.cur.execute("INSERT INTO FillTextQuestion(TASK_ID, QUESTION_ID, CORRECT_ANSWER) VALUES (?, ?, ?)", (task_id, question_id, answer))
+        self.cur.execute(
+            "INSERT INTO FillTextQuestion(TASK_ID, QUESTION_ID, CORRECT_ANSWER) VALUES (?, ?, ?)",
+            (task_id, question_id, answer),
+        )
         self.con.commit()
 
-    def get_fill_text_questions(self, task_id: int, question_id: int) -> list[FillTextQuestion]:
-        return self.cur.execute("SELECT * FROM FillTextQuestion WHERE TASK_ID = ? AND QUESTION_ID = ?", (task_id, question_id)).fetchall()
+    def get_fill_text_questions(
+        self, task_id: int, question_id: int
+    ) -> list[FillTextQuestion]:
+        return self.cur.execute(
+            "SELECT * FROM FillTextQuestion WHERE TASK_ID = ? AND QUESTION_ID = ?",
+            (task_id, question_id),
+        ).fetchall()
 
     # def insert_essay_question(self, question: str, answer: str):
     #     pass
     #
     # def insert_email_question(self, question: str, answer: str):
     #     pass
-
-
-
--- infra/sqlite/tasks.py
+++ infra/sqlite/tasks.py
@@ -9,8 +9,16 @@ from core.question import (
     EmailQuestion,
     EssayQuestion,
 )
-from core.task import Task, MatchParagraphsTask, ListeningTask, ReadingTask, FillTextTask, EssayTask, EmailTask, \
-    FillTextWithoutOptionsTask
+from core.task import (
+    Task,
+    MatchParagraphsTask,
+    ListeningTask,
+    ReadingTask,
+    FillTextTask,
+    EssayTask,
+    EmailTask,
+    FillTextWithoutOptionsTask,
+)
 
 
 @dataclass
@@ -26,16 +34,16 @@ class TasksDatabase:
         ).fetchall()
         tasks = []
         for (
-                id,
-                task_num,
-                task_title,
-                task_text,
-                task_options,
-                point,
-                task_type,
-                year,
-                variant,
-                subject
+            id,
+            task_num,
+            task_title,
+            task_text,
+            task_options,
+            point,
+            task_type,
+            year,
+            variant,
+            subject,
         ) in tasks_info:
             questions = []
             match task_type:
@@ -44,24 +52,35 @@ class TasksDatabase:
                     for id, t_id, paragraph, correct_answers in questions_info:
                         question = OpenQuestion(paragraph, correct_answers)
                         questions.append(question)
-                    task_1 = MatchParagraphsTask(questions, task_text.split('#'))
+                    task_1 = MatchParagraphsTask(questions, task_text.split("#"))
                 case "reading":
-                    questions_info = self.get_questions_info("MultipleChoiceQuestion", id)
-                    for id, t_id, question_text, question_options, question_answer in questions_info:
-                        question = MultipleChoiceQuestion(question_text, question_options.split('#'),
-                                                          question_answer)
+                    questions_info = self.get_questions_info(
+                        "MultipleChoiceQuestion", id
+                    )
+                    for (
+                        id,
+                        t_id,
+                        question_text,
+                        question_options,
+                        question_answer,
+                    ) in questions_info:
+                        question = MultipleChoiceQuestion(
+                            question_text, question_options.split("#"), question_answer
+                        )
                         questions.append(question)
                     task_1 = ReadingTask(questions, task_text)
                 case "filling":
                     questions_info = self.get_questions_info("FillTextQuestion", id)
-                    for id,t_id,  question_answer in questions_info:
+                    for id, t_id, question_answer in questions_info:
                         question = FillTextQuestion(question_answer)
                         questions.append(question)
-                    task_1 = FillTextTask(questions, task_text, task_options.split('#'))
+                    task_1 = FillTextTask(questions, task_text, task_options.split("#"))
                 case "articles":
-                    questions_info = self.get_questions_info("FillWithArticlesQuestion", id)
+                    questions_info = self.get_questions_info(
+                        "FillWithArticlesQuestion", id
+                    )
                     for id, t_id, question_answer in questions_info:
-                        question = FillWithArticlesQuestion(question_answer.split('#'))
+                        question = FillWithArticlesQuestion(question_answer.split("#"))
                         questions.append(question)
                     task_1 = FillTextWithoutOptionsTask(questions, task_text)
                 case "email":
@@ -71,10 +90,19 @@ class TasksDatabase:
                     essey_question = EssayQuestion()
                     task_1 = EssayTask([essey_question], task_text)
                 case "listening":
-                    questions_info = self.get_questions_info("MultipleChoiceQuestion", id)
-                    for id, t_id, question_text, question_options, question_answer in questions_info:
-                        question = MultipleChoiceQuestion(question_text, question_options.split('#'),
-                                                          question_answer)
+                    questions_info = self.get_questions_info(
+                        "MultipleChoiceQuestion", id
+                    )
+                    for (
+                        id,
+                        t_id,
+                        question_text,
+                        question_options,
+                        question_answer,
+                    ) in questions_info:
+                        question = MultipleChoiceQuestion(
+                            question_text, question_options.split("#"), question_answer
+                        )
                         questions.append(question)
                     task_1 = ListeningTask(questions)
             task = Task(task_num, task_title, point, task_type, task_1)
@@ -82,18 +110,43 @@ class TasksDatabase:
         return tasks
 
     def get_questions_info(self, question_type: str, task_id: str):
-        sql_script = "SELECT * FROM " + question_type + " WHERE TASK_ID = " + str(task_id)
+        sql_script = (
+            "SELECT * FROM " + question_type + " WHERE TASK_ID = " + str(task_id)
+        )
         questions_info = self.cur.execute(sql_script).fetchall()
         return questions_info
 
-    def insert_task(self, task_num: int, task_title: str, task_text: str, task_options: str, point: int, task_type: str,
-                    year: int, variant: int, subject: str):
+    def insert_task(
+        self,
+        task_num: int,
+        task_title: str,
+        task_text: str,
+        task_options: str,
+        point: int,
+        task_type: str,
+        year: int,
+        variant: int,
+        subject: str,
+    ):
         self.cur.execute(
             "INSERT INTO TASKS(TASK_NUM, TASK_TITLE, TASK_TEXT, TASK_OPTIONS, POINT, TASK_TYPE, YEAR, VARIANT, SUBJECT) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            (task_num, task_title, task_text, task_options, point, task_type, year, variant, subject))
+            (
+                task_num,
+                task_title,
+                task_text,
+                task_options,
+                point,
+                task_type,
+                year,
+                variant,
+                subject,
+            ),
+        )
         self.con.commit()
 
-    def get_result_points(self, request: dict) -> list[tuple[int, list[tuple[int, str]]]]:
+    def get_result_points(
+        self, request: dict
+    ) -> list[tuple[int, list[tuple[int, str]]]]:
         subject = request["subject"]
         year = request["year"]
         variant = request["variant"]
@@ -102,6 +155,6 @@ class TasksDatabase:
 
         results = []
         for task in tasks:
-            results.append(task.task.get_result_points(answers[task.task_number-1]))
+            results.append(task.task.get_result_points(answers[task.task_number - 1]))
 
         return results
