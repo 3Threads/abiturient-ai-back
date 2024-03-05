eb756f0d478709e9345c31175425bb8dc9ed2ac8
GuRgEnA <agurg21@freeuni.edu.ge>
2024-03-06 00:27:06 +0400

    Add get tasks test

--- tests/sqlite/test_tasks.py
+++ tests/sqlite/test_tasks.py
@@ -10,6 +10,8 @@ from infra.constants import (
     FILLING,
     FILLING_WITHOUT_OPTIONS,
     EMAIL,
+    CONVERSATION,
+    ESSAY,
 )
 from infra.sqlite.database_connect import Database
 from infra.sqlite.exams import ExamsDatabase
@@ -148,7 +150,7 @@ def test_insert_and_get_conversation_task(db: Database) -> None:
         6,
         "title",
         6,
-        "conversation",
+        CONVERSATION,
         exam_id,
         "text_title",
         "text",
@@ -160,7 +162,7 @@ def test_insert_and_get_conversation_task(db: Database) -> None:
     assert task.task_number == 6
     assert task.task_title == "title"
     assert task.task_point == 6
-    assert task.task_type == "conversation"
+    assert task.task_type == CONVERSATION
     assert task.exam_id == exam_id
     assert task.questions == []
     assert task.text_title == "text_title"
@@ -199,15 +201,70 @@ def test_insert_and_get_essay_task(db: Database) -> None:
     exams_db.create_new_exam("english", 2021, 1)
     exam_id = exams_db.get_exam_id("english", 2021, 1)
     task_db = TasksDatabase(db.get_connection(), db.get_cursor())
-    task_id = task_db.insert_essay_task(8, "title", 8, "essay", exam_id, "essay_title")
+    task_id = task_db.insert_essay_task(8, "title", 8, ESSAY, exam_id, "essay_title")
     task = task_db.get_essay_task(task_id)
 
     assert task.task_id == task_id
     assert task.task_number == 8
     assert task.task_title == "title"
     assert task.task_point == 8
-    assert task.task_type == "essay"
+    assert task.task_type == ESSAY
     assert task.exam_id == exam_id
     assert task.questions == []
+
+    db.close_database()
+
+
+def test_get_tasks(db: Database) -> None:
+    exams_db = ExamsDatabase(db.get_connection(), db.get_cursor())
+    exams_db.create_new_exam("english", 2021, 1)
+    exam_id = exams_db.get_exam_id("english", 2021, 1)
+    task_db = TasksDatabase(db.get_connection(), db.get_cursor())
+    task_db.insert_listening_task(1, "title", 1, LISTENING, exam_id, "address", 5)
+    task_db.insert_match_paragraphs_task(
+        2, "title", 2, MATCHING, exam_id, "text_title", ["paragraph1", "paragraph2"]
+    )
+    task_db.insert_reading_task(3, "title", 3, READING, exam_id, "text_title", "text")
+    task_db.insert_fill_text_task(
+        4, "title", 4, FILLING, exam_id, "text_title", "text", ["option1", "option2"]
+    )
+    task_db.insert_fill_text_without_options_task(
+        5, "title", 5, FILLING_WITHOUT_OPTIONS, exam_id, "text_title", "text"
+    )
+    task_db.insert_conversation_task(
+        6,
+        "title",
+        6,
+        CONVERSATION,
+        exam_id,
+        "text_title",
+        "text",
+        ["dialog1", "dialog2", "dialog3"],
+    )
+    task_db.insert_email_task(
+        7, "title", 7, EMAIL, exam_id, "text_title", "text", ["where?", "when?", "why?"]
+    )
+    task_db.insert_essay_task(8, "title", 8, ESSAY, exam_id, "essay_title")
+    tasks = task_db.get_tasks(exam_id)
+
+    assert len(tasks) == 8
+    assert tasks[0].task_type == LISTENING
+    assert tasks[1].task_type == MATCHING
+    assert tasks[2].task_type == READING
+    assert tasks[3].task_type == FILLING
+    assert tasks[4].task_type == FILLING_WITHOUT_OPTIONS
+    assert tasks[5].task_type == CONVERSATION
+    assert tasks[6].task_type == EMAIL
+    assert tasks[7].task_type == ESSAY
+    for i in range(8):
+        assert tasks[i].task_number == i + 1
+    for i in range(8):
+        assert tasks[i].task_point == i + 1
+    for i in range(8):
+        assert tasks[i].task_title == "title"
+    for i in range(8):
+        assert tasks[i].exam_id == exam_id
+    for i in range(8):
+        assert tasks[i].questions == []
 
     db.close_database()
