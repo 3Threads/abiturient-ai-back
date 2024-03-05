3d2c9fb5ad563e3435943f0dad4d9317d6912026
GuRgEnA <agurg21@freeuni.edu.ge>
2024-03-05 23:46:37 +0400

    Add exams

--- infra/constants.py
+++ infra/constants.py
@@ -13,6 +13,7 @@ FAKE_RATE = 100.0
 ADMIN_API_KEY = "b42b6a94c86a51f016e0ee11d140de6824b81d2cadcea443370d0c49ce251789"
 
 LISTENING = "listening"
+MATCHING = "matching"
 READING = "reading"
 TITLING = "titling"
 FILLING = "filling"
